0e4510ed0dbd199180cde972e09dd8678f6b8bec
Alexey <alexey.vovk@simplinic.com>
2023-08-02 20:04:56 +0300

    add autotests for ship and sail classes methods

--- src/test/java/edu/praktikum/SailTest.java
+++ src/test/java/edu/praktikum/SailTest.java
@@ -1,11 +1,19 @@
 package edu.praktikum;
 
+import edu.praktikum.sprint6.Sail;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class SailTest {
 
     @Test
     public void getMaxPowerTest() {
+        Sail sail = new Sail(2);
 
+        int actualResult = sail.getMaxPower();
+        int expectedResult = 4;
+
+        assertEquals(expectedResult, actualResult);
     }
 }
--- src/test/java/edu/praktikum/ShipParamsTest.java
+++ src/test/java/edu/praktikum/ShipParamsTest.java
@@ -1,11 +1,49 @@
 package edu.praktikum;
 
+import edu.praktikum.sprint6.Sail;
+import edu.praktikum.sprint6.Ship;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class ShipParamsTest {
+
+    @Parameterized.Parameter
+    public int power;
+    @Parameterized.Parameter(1)
+    public int expectedResult;
+
+    @Mock
+    Sail sail;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Parameterized.Parameters(name = "Ship with sail power {0} should have max speed {1}")
+    public static Object[][] params() {
+        return new Object[][] {
+                { 4, 16 },
+                { 3, 12 },
+        };
+    }
 
     @Test
     public void getMaxSpeed() {
+        Ship ship = new Ship(sail);
 
+        Mockito.when(sail.getMaxPower()).thenReturn(power);
+
+        int actualResult = ship.getMaxSpeed();
+
+        assertEquals(expectedResult, actualResult);
     }
 }
